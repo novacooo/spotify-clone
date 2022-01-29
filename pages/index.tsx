dd5a3fb68f6a62758d446ce9774f6f000400d451
novacooo <jaceknowak226@gmail.com>
2022-01-29 18:10:10 +0100

    Ready project for coding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Head from 'next/head';
-import Sidebar from '../components/Sidebar';
+import Sidebar from 'components/Sidebar';
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>spotify-clone</title>
-      </Head>
+const Home = () => (
+  <div>
+    <Head>
+      <title>spotify-clone</title>
+    </Head>
 
-      <main>
-        <Sidebar />
-        {/* Center */}
-      </main>
+    <main>
+      <Sidebar />
+      {/* Center */}
+    </main>
 
-      <div>{/* Player */}</div>
-    </div>
-  );
-}
+    <div>{/* Player */}</div>
+  </div>
+);
+
+export default Home;
