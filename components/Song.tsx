0e245bc99a2b96d520a84d13cbd16dd795969536
novacooo <jaceknowak226@gmail.com>
2022-02-02 23:47:04 +0100

    Playing songs is now working

--- components/Song.tsx
+++ components/Song.tsx
@@ -1,6 +1,8 @@
 import { millisToMinutesAndSeconds } from '@lib/time';
+import { currentTrackIdState, isPlayingState } from 'atoms/songAtom';
 import useSpotify from 'hooks/useSpotify';
 import React from 'react';
+import { useRecoilState } from 'recoil';
 
 interface ISongProps {
   track: any;
@@ -9,12 +11,26 @@ interface ISongProps {
 
 const Song = ({ track, order }: ISongProps) => {
   const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+  const [isPLaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+  const playSong = () => {
+    setCurrentTrackId(track.id);
+    setIsPlaying(true);
+
+    spotifyApi.play({
+      uris: [track.uri],
+    });
+  };
 
   return (
-    <div className="grid cursor-pointer grid-cols-2 rounded-lg py-4 px-5 text-gray-500 hover:bg-gray-900">
+    <div
+      className="grid cursor-pointer grid-cols-2 rounded-lg py-3 px-5 text-gray-500 hover:bg-gray-900"
+      onClick={playSong}
+    >
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
-        <img className="h-10 w-10" src={track.album.images[0].url} alt="Track picture" />
+        <img className="h-12 w-12" src={track.album.images[0].url} alt="Track picture" />
         <div>
           <p className="w-36 truncate text-white lg:w-64">{track.name}</p>
           <p className="w-40">{track.artists[0].name}</p>
