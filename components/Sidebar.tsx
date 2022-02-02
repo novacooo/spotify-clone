a3e5a866911925028ad0913cda15a3fa070a36cc
novacooo <jaceknowak226@gmail.com>
2022-02-02 12:04:47 +0100

    Playlists are now visible

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+// eslint-disable-next-line spaced-comment
+// <reference types="spotify-api" />
+
+import React, { useEffect, useState } from 'react';
 import {
   HeartIcon,
   HomeIcon,
@@ -8,72 +11,72 @@ import {
   RssIcon,
   SearchIcon,
 } from '@heroicons/react/outline';
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
+import useSpotify from 'hooks/useSpotify';
 
-const Sidebar = () => (
-  <div className="h-screen overflow-y-scroll border-r border-gray-900 p-5 pr-10 text-sm text-gray-500 scrollbar-hide">
-    <div className="space-y-4">
-      <button className="flex items-center space-x-2 hover:text-white" onClick={() => signOut()}>
-        <LogoutIcon className="h-5 w-5" />
-        <p>Sign out</p>
-      </button>
-      <button className="flex items-center space-x-2 hover:text-white">
-        <HomeIcon className="h-5 w-5" />
-        <p>Home</p>
-      </button>
-      <button className="flex items-center space-x-2 hover:text-white">
-        <SearchIcon className="h-5 w-5" />
-        <p>Search</p>
-      </button>
-      <button className="flex items-center space-x-2 hover:text-white">
-        <LibraryIcon className="h-5 w-5" />
-        <p>Your Library</p>
-      </button>
+const Sidebar = () => {
+  const spotifyApi = useSpotify();
+  const { data: session } = useSession();
+  const [playlists, setPLaylists] = useState<any>([]);
+  const [playlistId, setPlaylistId] = useState<string>();
 
-      <hr className="border-t-[0.1px] border-gray-900" />
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPLaylists(data.body.items);
+      });
+    }
+  }, [session, spotifyApi]);
 
-      <button className="flex items-center space-x-2 hover:text-white">
-        <PlusCircleIcon className="h-5 w-5" />
-        <p>Create Playlist</p>
-      </button>
-      <button className="flex items-center space-x-2 hover:text-white">
-        <HeartIcon className="h-5 w-5" />
-        <p>Liked Songs</p>
-      </button>
-      <button className="flex items-center space-x-2 hover:text-white">
-        <RssIcon className="h-5 w-5" />
-        <p>Your Episodes</p>
-      </button>
+  return (
+    <div className="h-screen overflow-y-scroll border-r border-gray-900 p-5 pr-10 text-sm text-gray-500 scrollbar-hide">
+      <div className="space-y-4">
+        <button className="flex items-center space-x-2 hover:text-white" onClick={() => signOut()}>
+          <LogoutIcon className="h-5 w-5" />
+          <p>Sign out</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="h-5 w-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="h-5 w-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="h-5 w-5" />
+          <p>Your Library</p>
+        </button>
 
-      <hr className="border-t-[0.1px] border-gray-900" />
+        <hr className="border-t-[0.1px] border-gray-900" />
 
-      {/* Playlists */}
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
-      <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <PlusCircleIcon className="h-5 w-5" />
+          <p>Create Playlist</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HeartIcon className="h-5 w-5" />
+          <p>Liked Songs</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <RssIcon className="h-5 w-5" />
+          <p>Your Episodes</p>
+        </button>
+
+        <hr className="border-t-[0.1px] border-gray-900" />
+
+        {playlists.map((playlist: any) => (
+          <p
+            key={playlist.id}
+            onClick={() => setPlaylistId(playlist.id)}
+            className="cursor-pointer hover:text-white"
+          >
+            {playlist.name}
+          </p>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Sidebar;
